af699abbb2460994ad7b631993d8a7f52c1b6e72
Dhanesh <dhanesh2435@gmail.com>
2026-05-16 12:00:39 +0530

    Css changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import "./styles/global.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,7 +6,6 @@ import { Toaster } from "react-hot-toast";
 import ScrollToTop from "./components/common/ScrollToTop";
 
 import App from "./App";
-import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
